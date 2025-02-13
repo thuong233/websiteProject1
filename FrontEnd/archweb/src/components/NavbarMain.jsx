725ad19e11c17e58a7b7af5edc76166782deeac0
jmoji27 <jaimejesus@ogr.eskisehir.edu.tr>
2025-02-13 06:48:49 +0300

    'frontend1?'

---
+++ FrontEnd/archweb/src/components/NavbarMain.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavbarMain = () => {
+  return <div>NavbarMain</div>;
+};
+
+export default NavbarMain;
---
+++ FrontEnd/archweb/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {brown: "#53423e",
+      lightBrown: "#645550",
+      darkBrown: "#2c2523",
+      black: "#1e1917",
+      white: "#f1e1d9",
+      cyan: "#15d1e9",
+      lightCyan: "#88e5f0",
+      darkCyan: "#009fb3",
+      orange: "#fb9718",
+      lightOrange: "#fac27b",
+      darkOrange: "#d28422",
+      grey: "#626965",
+      lightGrey: "#978580",
+      darkGrey: "#3f4441",},
+    extend: {},
+    fontFamily:{
+      body:["Josefin Sans'"],
+      special: ["Roboto"],
+    }
+  },
+  plugins: [],
+}
+
