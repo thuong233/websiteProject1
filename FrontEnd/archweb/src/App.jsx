d2692c74a616f56ebffbfbce23919617a104368d
thuongdev23 <thuongvannguyen233@gmail.com>
2025-05-27 16:58:24 +0700

    fix subherosection

--- FrontEnd/archweb/src/App.jsx
+++ FrontEnd/archweb/src/App.jsx
@@ -1,6 +1,6 @@
 import NavbarMain from "./components/navbar/NavbarMain";
 import HeroMain from "./components/heroSection/HeroMain";
-import SubHeroSection from "./components/heroSection/subHeroSection";
+// import SubHeroSection from "./components/heroSection/subHeroSection";
 import AboutMeMain from "./components/aboutMeSection/AboutMeMain";
 import HelperSection from "./components/HelperSection";
 import SkillsMain from "./components/specialSection/SkillsMain";
@@ -9,7 +9,7 @@ import SubSkills from "./components/specialSection/SubSkills";
 import ProjectsMain from "./components/projectsSection/ProjectsMain";
 import ContactMeMain from "./components/contactMeSection/ContactMeMain";
 import FooterMain from "./components/footer/FooterMain";
-
+import SubHeroSection from "./components/heroSection/SubHeroSection";
 function App() {
   return (
     <main className="font-body">
